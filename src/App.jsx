0b00ba3130a2b6ae8e83ec898db16ad589299735
dipanshusolanki <deepanshusolanki@gmail.com>
2024-07-26 11:45:16 +0530

    C3: Button

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <button className="feedback">Give Feedback</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
